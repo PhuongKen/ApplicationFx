c4ff9cf13365d0d4f95881e172c3da2fff95ba64
PhuongKen <PhuongNMD00539@fpt.edu.vn>
2018-06-05 11:23:21 +0700

    Sua txtImage

--- src/javaFX/FormProduct.java
+++ src/javaFX/FormProduct.java
@@ -7,10 +7,18 @@ package javaFX;
 
 import entity.Product;
 import java.awt.Desktop;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -33,7 +41,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import javax.imageio.ImageIO;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import model.ProductModel;
+import org.w3c.dom.Document;
 
 /**
  *
@@ -100,20 +113,36 @@ public class FormProduct extends Application {
                 return new TableCell<Product, String>() {
                     @Override
                     public void updateItem(String item, boolean empty) {
+                        ArrayList<String> listImage = new ArrayList<>();
                         if (item != null) {
-                            HBox box = new HBox();
-                            box.setSpacing(10);
-                            VBox vbox = new VBox();
-                            ImageView imageView = new ImageView();
-                            imageView.setFitHeight(50);
-                            imageView.setFitWidth(50);
-                            String imgString = model.queryImage(item);
-                            String[] splitArray = imgString.split("src");
-                            String i = splitArray[1].trim();
-                            System.out.println(i);
-                            imageView.setImage(new Image(i));
-                            box.getChildren().addAll(imageView, vbox);
-                            setGraphic(box);
+                            InputStream input = null;
+                            try {
+                                HBox box = new HBox();
+                                box.setSpacing(10);
+                                VBox vbox = new VBox();
+                                String imgString = model.queryImage(item);
+                                input = new FileInputStream(imgString);
+                                Image image = new Image(input);
+                                ImageView imageView = new ImageView(image);
+                                imageView.setFitHeight(50);
+                                imageView.setFitWidth(50);
+                                //                            String[] splitArray = imgString.split("src");
+//                            String i = splitArray[1].trim();
+//                            System.out.println(i);
+//                                imageView.setImage(new Image(imgString));
+                                box.getChildren().addAll(imageView, vbox);
+                                setGraphic(box);
+                            } catch (FileNotFoundException ex) {
+                                Logger.getLogger(FormProduct.class.getName()).log(Level.SEVERE, null, ex);
+                            } catch (IOException ex) {
+                                Logger.getLogger(FormProduct.class.getName()).log(Level.SEVERE, null, ex);
+                            } finally {
+                                try {
+                                    input.close();
+                                } catch (IOException ex) {
+                                    Logger.getLogger(FormProduct.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                            }
                         }
                     }
                 };
@@ -121,51 +150,102 @@ public class FormProduct extends Application {
 
         });
 
-        tableView.getColumns().addAll(columnImage, columnName, columnPrice, columnAction);
+        tableView.getColumns()
+                .addAll(columnImage, columnName, columnPrice, columnAction);
 
         //
         Button submit = new Button("Submit");
-        submit.setOnAction((ActionEvent event) -> {
-            String name = txtName.getText();
-            String image = txtImage.getText();
-            String price = txtPrice.getText();
-            Product p = new Product(name, image, Integer.parseInt(price));
-            model.save(p);
-            System.out.println("Lưu thành công.");
-            ObservableList<Product> list = model.query();
-            tableView.getItems().setAll(list);
-            window.setScene(scene2);
+
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+            boolean daluu = false;
+
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    BufferedImage image1 = null;
+                    String name = txtName.getText();
+                    String imageUrl = txtImage.getText();
+                    if (imageUrl.contains("http")) {
+                        URL url = new URL(txtImage.getText());
+                        image1 = ImageIO.read(url);
+                        ImageIO.write(image1, "jpg", new File("E:\\Java\\ApplicationFx\\src\\resource\\" + txtName.getText() + ".jpg"));
+                        daluu = true;
+                        if (daluu) {
+                            String image = "E:\\Java\\ApplicationFx\\src\\resource\\" + txtName.getText() + ".jpg";
+                            String price = txtPrice.getText();
+                            Product p = new Product(name, image, Integer.parseInt(price));
+                            model.save(p);
+                            System.out.println("Lưu thành công.");
+                            ObservableList<Product> list = model.query();
+                            tableView.getItems().setAll(list);
+                            window.setScene(scene2);
+                            daluu = false;
+                        }
+                    } else {
+                        String price = txtPrice.getText();
+                        Product p = new Product(name, imageUrl, Integer.parseInt(price));
+                        model.save(p);
+                        System.out.println("Lưu thành công.");
+                        ObservableList<Product> list = model.query();
+                        tableView.getItems().setAll(list);
+                        window.setScene(scene2);
+                    }
+
+                } catch (MalformedURLException ex) {
+                    Logger.getLogger(FormProduct.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(FormProduct.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         });
         Button reset = new Button("Reset");
 
-        reset.setOnAction((event) -> {
-            txtName.clear();
-            txtImage.clear();
-            txtPrice.clear();
-        });
+        reset.setOnAction(
+                (event) -> {
+                    txtName.clear();
+                    txtImage.clear();
+                    txtPrice.clear();
+                }
+        );
 
-        GridPane.setConstraints(lblName, 2, 2);
-        GridPane.setConstraints(lblImage, 2, 3);
-        GridPane.setConstraints(lblPrice, 2, 4);
+        GridPane.setConstraints(lblName,
+                2, 2);
+        GridPane.setConstraints(lblImage,
+                2, 3);
+        GridPane.setConstraints(lblPrice,
+                2, 4);
 
-        GridPane.setConstraints(txtName, 3, 2);
-        GridPane.setConstraints(txtImage, 3, 3);
-        GridPane.setConstraints(chooser, 4, 3);
-        GridPane.setConstraints(txtPrice, 3, 4);
+        GridPane.setConstraints(txtName,
+                3, 2);
+        GridPane.setConstraints(txtImage,
+                3, 3);
+        GridPane.setConstraints(chooser,
+                4, 3);
+        GridPane.setConstraints(txtPrice,
+                3, 4);
 
         HBox hBox = new HBox();
-        hBox.setSpacing(10);
-        hBox.getChildren().addAll(submit, reset);
-        GridPane.setConstraints(hBox, 3, 5);
 
-        pane1.getChildren().addAll(lblName, lblImage, lblPrice, txtName, txtImage, chooser, txtPrice, hBox);
+        hBox.setSpacing(
+                10);
+        hBox.getChildren()
+                .addAll(submit, reset);
+        GridPane.setConstraints(hBox,
+                3, 5);
+
+        pane1.getChildren()
+                .addAll(lblName, lblImage, lblPrice, txtName, txtImage, chooser, txtPrice, hBox);
         pane1.setAlignment(Pos.CENTER);
 
         Product p = new Product();
 
         scene2 = new Scene(tableView, 400, 300);
+//        ObservableList<Product> list = model.query();
+//        tableView.getItems().setAll(list);
         scene1 = new Scene(pane1, 450, 280);
+
         primaryStage.setScene(scene1);
+
         primaryStage.show();
     }
 
